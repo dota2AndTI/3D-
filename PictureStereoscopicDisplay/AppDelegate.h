e0d906deee38bb446f8591c3e8fa240a116cc4f2
caoyulong <caoyulong@ivymobile.com>
2018-05-28 14:25:31 +0800

    Initial Commit

---
+++ PictureStereoscopicDisplay/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PictureStereoscopicDisplay
+//
+//  Created by Ivy on 2018/5/28.
+//  Copyright © 2018年 CYL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PictureStereoscopicDisplay/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PictureStereoscopicDisplay
+//
+//  Created by Ivy on 2018/5/28.
+//  Copyright © 2018年 CYL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
